b400f5e8505d5c7d0d1c629e61d414df128211a1
AlexMukha <alexmukha@email.com>
2019-01-18 16:35:04 -0800

    Working random Qs API

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,45 @@
-var trivia;
-// timeLeft:
+$(document).ready(function() {
+  var timeLeft = 10;
+  var timerId = setInterval(countdown, 1000);
+  
+  
+  function start(){
+    var $start = $('<button class="btn">Start</button>');
+    $start.on('click', countdown);
+    // $countdown.preventDefault();
+    // $app.empty();
+    // $app.append($start);
+    correct = 0;
+    incorrect = 0;
+    // questionIndex = 0;
+    // clearTimeout(timerId);
+    // timeLeft = 0;
+    $("#main").append($start);
+}
+start();
 
 
 
-$("#main").on("click", function() {
-    delayButtonAlert = setTimeout(function() {
-      alert("button is clicked.");
-    }, 3000);
-  });
+
+  function checkResults() {
+    $("#timer").removeClass("warning").addClass("normal");
+    $("#timer").html("Time\'s Up");
+ }
+
+  
+  function countdown() {
+      if (timeLeft == -0) {
+          clearTimeout(timerId);
+          checkResults();
+      } else {
+        if (timeLeft < 5) {
+          $("#timer").removeClass("normal").addClass("warning");
+        }
+          $("#timer").html("You have "+timeLeft+" seconds to answer the question!");
+          timeLeft--;
+      }
+  }
+  
 
 
+});
